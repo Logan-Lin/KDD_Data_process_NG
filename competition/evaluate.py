3129321e10446895041b03d5380b3f090525132d
Logan Lin <juniormail@126.com>
2018-05-08 09:49:54 +0800

    Modified evaluate algorithm

--- competition/evaluate.py
+++ competition/evaluate.py
@@ -16,16 +16,10 @@ def smape(actual, predicted):
     return 2 * np.mean(np.divide(a, b, out=np.zeros_like(a), where=b != 0, casting='unsafe'))
 
 
-def file_smape(predict_dt, file_suffix=None, predict_csv=None):
-    """
-    Calculate smape based on predict submission file and real data from api,
-    output csv file for easy comparision at the same time.
+def cal_smape(predict_dt, file_suffix=None, predict_csv=None, city="bj"):
+    predict_column = {"bj": ["PM2.5", "PM10", "O3"], "ld": ["PM2.5", "PM10"]}[city]
+    real_column = {"bj": ["pm2.5", "pm10", "o3"], "ld": ["pm2.5", "pm10"]}[city]
 
-    :param predict_dt: The start date of the predict
-    :param file_suffix: The suffix you want to add to the output comparision file.
-    :param predict_csv: Appoint for the predict submission file.
-        If left None, will open default format submission file.
-    """
     predict_dt = dt.strptime(predict_dt, format_string[1])
     if predict_csv is None:
         predict_df = pd.read_csv("../submit/sample_submission_{}.csv".format(predict_dt.strftime(format_string[2])),
@@ -36,68 +30,69 @@ def file_smape(predict_dt, file_suffix=None, predict_csv=None):
     t.index = predict_df.index.copy()
     predict_df['prefix'] = t
 
-    split_index = predict_df[predict_df["test_id"] == "BL0#0"].index.values.astype(int)[0]
-    bj_predict = predict_df[:split_index]
-    ld_predict = predict_df[split_index:]
-    ld_predict = ld_predict.drop("O3", axis=1)
-
-    bj_true_data = load_data.load_directory_data(load_data.api_data_directory.format("bj", "aq"),
-                                                 data_header=load_data.data_header_dict["bj"]["aq"],
-                                                 export_none=True)
-    ld_true_data = load_data.load_directory_data(load_data.api_data_directory.format("ld", "aq"),
-                                                 data_header=load_data.data_header_dict["ld"]["aq"],
+    bj_true_data = load_data.load_directory_data(load_data.api_data_directory.format(city, "aq"),
+                                                 data_header=load_data.data_header_dict[city]["aq"],
                                                  export_none=True)
     aq_dfs = []
-    for aq_name in bj_raw_fetch.aq_location.keys():
-        true_span_df = bj_true_data.loc[aq_name, :].loc[predict_dt:predict_dt + td(hours=47), ["pm2.5", "pm10", "o3"]]
+    aq_names = {"bj": bj_raw_fetch.aq_location.keys(), "ld": ld_raw_fetch.aq_location.keys()}[city]
+    for aq_name in aq_names:
+        true_span_df = bj_true_data.loc[aq_name, :].loc[predict_dt:predict_dt + td(hours=47), real_column]
         true_span_df = true_span_df.reset_index()
-        predict_span_df = bj_predict[bj_predict["prefix"] == aq_name]
+        predict_span_df = predict_df[predict_df["prefix"] == aq_name]
         predict_span_df = predict_span_df.reset_index()
         merge_df = pd.concat([predict_span_df, true_span_df], axis=1)
         aq_dfs.append(merge_df)
-    bj_concat = pd.concat(aq_dfs).reset_index()
-
-    aq_dfs = []
-    for aq_name in ld_raw_fetch.aq_location.keys():
-        true_span_df = ld_true_data.loc[aq_name, :].loc[predict_dt:predict_dt + td(hours=47), ["pm2.5", "pm10"]]
-        true_span_df = true_span_df.reset_index()
-        predict_span_df = ld_predict[ld_predict["prefix"] == aq_name]
-        predict_span_df = predict_span_df.reset_index()
-        merge_df = pd.concat([predict_span_df, true_span_df], axis=1)
-        aq_dfs.append(merge_df)
-    ld_concat = pd.concat(aq_dfs).reset_index()
+    concat_df = pd.concat(aq_dfs).reset_index()
+    if city == "ld":
+        concat_df = concat_df.drop(["O3"], axis=1)
 
     csv_directory = os.path.join("compare", predict_dt.strftime(format_string[2]))
+    file_name = city + "_"
     if file_suffix is not None:
-        csv_directory = os.path.join(csv_directory, file_suffix)
+        file_name += file_suffix
     if not os.path.exists(csv_directory):
         os.makedirs(csv_directory)
-    bj_concat.drop(["level_0", "index", "prefix"], axis=1).to_csv(os.path.join(csv_directory, "bj.csv"), sep=",")
-    ld_concat.drop(["level_0", "index", "prefix"], axis=1).to_csv(os.path.join(csv_directory, "ld.csv"), sep=",")
+    concat_df.to_csv(os.path.join(csv_directory, file_name + ".csv"), sep=",")
 
-    bj_concat = bj_concat.dropna(how="any", axis=0)
-    ld_concat = ld_concat.dropna(how="any", axis=0)
+    concat_df = concat_df.dropna(how="any", axis=0)
 
-    bj_all_predict = np.array(bj_concat.loc[:, ["PM2.5", "PM10", "O3"]])
-    bj_all_real = np.array(bj_concat.loc[:, ["pm2.5", "pm10", "o3"]])
+    predict_str = predict_dt.strftime(format_string[1])
+    column_smapes = []
+    for i in range(len(predict_column)):
+        all_predict = np.array(concat_df.loc[:, predict_column[i]])
+        all_real = np.array(concat_df.loc[:, real_column[i]])
 
-    ld_all_predict = np.array(ld_concat.loc[:, ["PM2.5", "PM10"]])
-    ld_all_real = np.array(ld_concat.loc[:, ["pm2.5", "pm10"]])
+        smape_value = smape(all_real, all_predict)
+        column_smapes.append(smape_value)
+        print("{} {} {} - {}".format(city, predict_str, predict_column[i], smape_value))
 
-    # all_predict = np.vstack([bj_all_predict.reshape(-1, 1), ld_all_predict.reshape(-1, 1)]).T
-    # all_real = np.vstack([bj_all_real.reshape(-1, 1), ld_all_real.reshape(-1, 1)]).T
-    # all_smape = smape(all_real, all_predict)
-    # print("Average smape {}".format(all_smape))
+    all_predict = np.array(concat_df.loc[:, predict_column])
+    all_real = np.array(concat_df.loc[:, real_column])
+    smape_value = smape(all_real, all_predict)
+    column_smapes.append(smape_value)
+    print("{} {} Aggregate - {}".format(city, predict_str, smape_value))
 
-    bj_size = np.shape(bj_all_predict)[0]
-    ld_size = np.shape(ld_all_predict)[0]
-
-    bj_smape = smape(bj_all_real, bj_all_predict)
-    ld_smape = smape(ld_all_real, ld_all_predict)
-    print("Beijing {} - {}".format(predict_dt, bj_smape))
-    print("London {} - {}".format(predict_dt, ld_smape))
-    print("Average {}".format((bj_smape + ld_smape) / 2))
+    return [file_name] + column_smapes
 
 
 if __name__ == "__main__":
-    file_smape("2018-05-04-00")
+    table = []
+    for root, directories, filenames in os.walk("../submit"):
+        for filename in filenames:
+            name, ext = os.path.splitext(filename)
+            if ext == ".csv":
+                print(name)
+                file_dir = os.path.join(root, filename)
+                bj_smape = cal_smape(
+                    predict_dt=dt.strptime((name.split("_"))[0], format_string[2]).strftime(format_string[1]),
+                    predict_csv=file_dir, file_suffix=name, city="bj")
+                ld_smape = cal_smape(
+                    predict_dt=dt.strptime((name.split("_"))[0], format_string[2]).strftime(format_string[1]),
+                    predict_csv=file_dir, file_suffix=name, city="ld")
+                table.append(bj_smape + ld_smape[1:])
+                print()
+    df = pd.DataFrame(table, columns=["file_name", "pm2.5_bj", "pm10_bj", "o3_bj",
+                                      "aggregate_bj", "pm2.5_ld", "pm10_ld", "aggregate_ld"])
+    df = df.set_index("file_name")
+    df = df.sort_index()
+    df.to_csv("compare.csv", sep=",")
--- traditional/data_export.py
+++ traditional/data_export.py
@@ -6,15 +6,16 @@ import math
 import os
 from datetime import datetime, timedelta
 from time import sleep
+from competition import load_data
 
 import h5py
 import numpy as np
 
 from forecast import parse
-from utils import ld_raw_fetch, export_statistic
+from utils import ld_raw_fetch, export_statistic, bj_raw_fetch
 from utils.tools import per_delta, get_one_hot, angle_to_int, str2bool
 
-time_span = 24
+history_span = 24
 predict_span = 50
 
 
@@ -72,7 +73,8 @@ def get_fake_forecast_data(aq_name, dt_string):
 
 
 def get_forecast_data(dt_object):
-    file_directory = "../forecast/data/bj_{}.txt".format((dt_object + timedelta(hours=8)).strftime("%m_%d_%H"))
+    file_dir = {"bj": "../forecast_data/ld_{}.txt", "ld": "../forecast_data/bj_{}.txt"}[ci]
+    file_directory = file_dir.format((dt_object + timedelta(hours=8)).strftime("%m_%d_%H"))
     return parse.get_data(file_directory)
 
 
@@ -85,15 +87,21 @@ def check_valid(aq_name, start_object):
             forecast_matrix = get_forecast_data(start_object)
         except FileNotFoundError:
             need_fake = True
-        for i in range(time_span - 1, -1, -1):
+        for i in range(history_span - 1, -1, -1):
             valid_dt_string = (start_object - timedelta(hours=i)).strftime(format_string)
             history_aq_matrix.append(aq_dict[valid_dt_string])
             history_meo_matrix.append(grid_dicts[(get_nearest(aq_location[aq_name]))[0]][valid_dt_string])
         for i in range(1, predict_span + 1):
             predict_dt_string = (start_object + timedelta(hours=i)).strftime(format_string)
-            statistic_matrix.append(export_statistic.get_month_data(aq_name, predict_dt_string))
+            column = None
+            if ci == "bj":
+                column="o3"
+            statistic_matrix.append(export_statistic.get_month_data(aq_name, predict_dt_string, df, column=column))
             if export_predict:
-                predict_aq_matrix.append(aq_dict[predict_dt_string])
+                if ci == "bj":
+                    predict_aq_matrix.append([aq_dict[predict_dt_string][4]])
+                else:
+                    predict_aq_matrix.append(aq_dict[predict_dt_string])
             if need_fake:
                 forecast_matrix.append(get_fake_forecast_data(aq_name, predict_dt_string))
     except KeyError:
@@ -126,16 +134,39 @@ for i in range(len(holiday_array)):
 
 
 # Export data
-def export_data(read_start_string, read_end_string, export_start_string, export_end_string,
+def export_data(city, read_start_string, read_end_string, export_start_string, export_end_string,
                 use_fill, use_history, export_train):
     start_string, end_string = read_start_string, read_end_string
-    global aq_location, grid_location, aq_dicts, grid_dicts
+    global ci
+    ci = city
+    global aq_location, grid_location, aq_dicts, grid_dicts, df
     if use_history:
-        aq_location, grid_location, aq_dicts, grid_dicts = ld_raw_fetch.load_all_history()
+        if city == "ld":
+            aq_location, grid_location, aq_dicts, grid_dicts = ld_raw_fetch.load_all_history()
+            df = load_data.load_directory_data(load_data.history_data_directory[city]["aq"],
+                                               load_data.data_header_dict[city]["aq"], drop=["no2"])
+        elif city == "bj":
+            aq_location, grid_location, aq_dicts, grid_dicts = bj_raw_fetch.load_all_history()
+            df = load_data.load_directory_data(load_data.history_data_directory[city]["aq"],
+                                               load_data.data_header_dict[city]["aq"])
+
     else:
-        aq_location, grid_location, aq_dicts, grid_dicts = ld_raw_fetch.load_all(start_string, end_string)
+        if city == "ld":
+            aq_location, grid_location, aq_dicts, grid_dicts = ld_raw_fetch.load_all(start_string, end_string)
+            df = load_data.load_directory_data([load_data.filled_data_directory[city],
+                                                load_data.history_data_directory[city]["aq"]],
+                                               load_data.data_header_dict[city]["aq"], drop=["no2"])
+        elif city == "bj":
+            aq_location, grid_location, aq_dicts, grid_dicts = bj_raw_fetch.load_all(start_string, end_string)
+            df = load_data.load_directory_data([load_data.filled_data_directory[city],
+                                                load_data.history_data_directory[city]["aq"]],
+                                               load_data.data_header_dict[city]["aq"])
+
         if use_fill:
-            aq_dicts = ld_raw_fetch.load_filled_dicts(start_string, end_string)
+            if city == "ld":
+                aq_dicts = ld_raw_fetch.load_filled_dicts(start_string, end_string)
+            elif city == "bj":
+                aq_dicts = bj_raw_fetch.load_filled_dicts(start_string, end_string)
     global export_predict
     export_predict = export_train
 
@@ -146,29 +177,39 @@ def export_data(read_start_string, read_end_string, export_start_string, export_
     diff = end_datetime - start_datetime
     days, seconds = diff.days, diff.seconds
     delta_time = int(days * 24 + seconds // 3600)
-
     if export_train:
-        directory = "../data_ld/tradition_train/{}_{}".format(start_string, end_string)
+        delta_time = int(delta_time / 24)
+
+    directory = ""
+    if export_train:
+        if city == "ld":
+            directory = "../data_ld/tradition_train/{}_{}".format(start_string, end_string)
+        elif city == "bj":
+            directory = "../data/tradition_train/{}_{}".format(start_string, end_string)
     else:
-        directory = "../data_ld/tradition_predict/{}_{}".format(start_string, end_string)
+        if city == "ld":
+            directory = "../data_ld/tradition_predict/{}_{}".format(start_string, end_string)
+        elif city == "bj":
+            directory = "../data/tradition_predict/{}_{}".format(start_string, end_string)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    print("\nFetching data to export...")
-    out_file = open("out{}_{}.txt".format(start_string, end_string), "w")
+    print("\nExporting to {}".format(directory))
+    # out_file = open("out{}_{}.txt".format(start_string, end_string), "w")
     for aq_name in aq_location.keys():
-        # if aq_name not in ["CD1"]:
+        # if aq_name not in ["KF1"]:
         #     continue
         timestamp_matrix, history_aq, history_meo, forecast, predict_aq, statistic = [], [], [], [], [], []
         if export_train:
             aggregate = 0
+            valid = 0
 
-            for dt_object in per_delta(start_datetime, end_datetime, timedelta(hours=1)):
+            for dt_object in per_delta(start_datetime, end_datetime, timedelta(hours=24)):
                 aggregate += 1
-                if aggregate % 100 == 0:
-                    print("{} exported %3.2f%%".format(aq_name) % (100 * aggregate / delta_time))
-                    out_file.write("{} exported %3.2f%%\n".format(aq_name) % (100 * aggregate / delta_time))
-                    out_file.flush()
+                if aggregate % 10 == 0:
+                    print("\t{} exported %3.2f%%".format(aq_name) % (100 * aggregate / delta_time))
+                #     out_file.write("{} exported %3.2f%%\n".format(aq_name) % (100 * aggregate / delta_time))
+                #     out_file.flush()
 
                 history_aq_matrix, history_meo_matrix, forecast_matrix, predict_matrix, \
                 weekday, weekend, timestamp, statistic_matrix = check_valid(aq_name, dt_object)
@@ -181,6 +222,7 @@ def export_data(read_start_string, read_end_string, export_start_string, export_
                 forecast.append(forecast_matrix)
                 predict_aq.append(predict_matrix)
                 statistic.append(statistic_matrix)
+                valid += 1
 
             h5_file = h5py.File("{}/{}.h5".format(directory, aq_name), "w")
             h5_file.create_dataset("timestamp", data=np.array(timestamp_matrix))
@@ -191,7 +233,7 @@ def export_data(read_start_string, read_end_string, export_start_string, export_
             h5_file.create_dataset("statistic", data=np.array(statistic))
             h5_file.flush()
             h5_file.close()
-            print(" {} finished".format(aq_name))
+            print("{} finished, valid {}".format(aq_name, valid))
             sleep(0.1)
         else:
             last_valid_dt = None
@@ -225,20 +267,22 @@ def export_data(read_start_string, read_end_string, export_start_string, export_
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--city", type=str,
+                        help="City, input 'bj' or 'ld' for Beijing and London", default="bj")
     parser.add_argument("-s", "--start", type=str,
-                        help="Start datetime string, in YYYY-MM-DD-hh format", default="2017-01-01-00")
+                        help="Start datetime string, in YYYY-MM-DD-hh format", default="2017-01-01-22")
     parser.add_argument("-e", "--end", type=str,
-                        help="End datetime string, in YYYY-MM-DD-hh format", default="2017-02-01-00")
+                        help="End datetime string, in YYYY-MM-DD-hh format", default="2018-04-21-22")
     parser.add_argument("-es", "--exportstart", type=str,
                         help="Start datetime to export, in YYYY-MM-DD-hh format", default=None)
     parser.add_argument("-ee", "--exportend", type=str,
                         help="End datetime to export, in YYYY-MM-DD-hh format", default=None)
     parser.add_argument("-f", "--fill", type=str2bool,
-                        help="Use filled data or not, input true/false", default=True)
+                        help="Use filled data or not, input true/false", default=False)
     parser.add_argument("-his", "--history", type=str2bool,
-                        help="Use history data, export train data", default=True)
+                        help="Use history data", default=True)
     parser.add_argument("-t", "--train", type=str2bool,
                         help="Export train data or predict data", default=True)
     argv = parser.parse_args()
 
-    export_data(argv.start, argv.end, argv.exportstart, argv.exportend, argv.fill, argv.history, argv.train)
+    export_data(argv.city, argv.start, argv.end, argv.exportstart, argv.exportend, argv.fill, argv.history, argv.train)
